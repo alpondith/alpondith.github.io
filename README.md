460754e48e04da03708b7539d995ce7aa1321739
alpondith <sujonalimran@gmail.com>
2024-10-17 16:06:42 +0600

    update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Commands
 
+```shell
+npm install
+npm run dev
+npm run build
+npm run preview
+```
 
 [Website](https://alpondith.github.io/)
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import nunjucks from "vite-plugin-nunjucks";
+
+export default {
+    plugins: [
+        nunjucks(),
+    ],
+    optimizeDeps: {
+        include: ["nunjucks"],
+    },
+    root: "src",
+    // base: "",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: "src/index.html",
+            },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: (file) => {
+                    const ext = file.name.split(".").pop();
+                    if (["png", "jpg", "jpeg", "gif", "svg", "ico"].includes(ext)) {
+                        return `assets/img/${file.name}`;
+                    }
+                    return `assets/${file.name}`;
+                },
+            },
+        },
+    },
+};
